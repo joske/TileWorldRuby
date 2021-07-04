9e6c8544b5ba950a5b0516565a5eb32b9957ca77
Jos Dehaes <jos.dehaes@gmail.com>
2021-07-04 20:15:30 +0200

    add test

--- tc_astar.rb
+++ tc_astar.rb
@@ -7,8 +7,9 @@ require 'test/unit'
 COLS = 5
 ROWS = 5
 
+# unit test for astar
 class TestPath < Test::Unit::TestCase
-  def testSearch
+  def test_search
     grid = Grid.new
     from = Location.new(0, 0)
     to = Location.new(1, 1)
@@ -19,7 +20,7 @@ class TestPath < Test::Unit::TestCase
     assert_equal(Location.new(1, 1), path[1])
   end
 
-  def testSearch2
+  def test_search2
     # S....
     # ####.
     # .....
@@ -44,5 +45,25 @@ class TestPath < Test::Unit::TestCase
     assert_equal(Location.new(4, 2), path[5])
     assert_equal(Location.new(4, 3), path[6])
     assert_equal(Location.new(4, 4), path[7])
+  end
+
+  def test_search3
+    # S....
+    # #####
+    # .....
+    # .....
+    # ....E
+    grid = Grid.new
+    from = Location.new(0, 0)
+    to = Location.new(4, 4)
+    obstacles = [Obstacle.new(0, Location.new(0, 1)), Obstacle.new(1, Location.new(1, 1)),
+                 Obstacle.new(2, Location.new(2, 1)), Obstacle.new(3, Location.new(3, 1)), 
+                 Obstacle.new(4, Location.new(4, 1))]
+    obstacles.each do |obst|
+      grid.set_object(obst.location, obst)
+    end
+    path = astar(grid, from, to)
+    puts "from #{from} to #{to} : #{path}"
+    assert_equal(0, path.size) # can not reach goal
   end
 end
